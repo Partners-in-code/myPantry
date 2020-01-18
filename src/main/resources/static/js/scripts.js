f4adfcf3cc12bc1d77ab8cfa5f4e3b695e5dfbe7
mariaalexab <49718025+mariaalexab@users.noreply.github.com>
2020-01-18 22:12:36 +0200

    Add files via upload

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,16 +2,15 @@
  * 
  */
 
-
 function checkPasswordMatch() {
 	var password = $("#txtNewPassword").val();
 	var confirmPassword = $("#txtConfirmPassword").val();
-	
-	if(password == "" && confirmPassword =="") {
+
+	if (password == "" && confirmPassword == "") {
 		$("#checkPasswordMatch").html("");
 		$("#updateUserInfoButton").prop('disabled', false);
 	} else {
-		if(password != confirmPassword) {
+		if (password != confirmPassword) {
 			$("#checkPasswordMatch").html("Passwords do not match!");
 			$("#updateUserInfoButton").prop('disabled', true);
 		} else {
@@ -21,7 +20,33 @@ function checkPasswordMatch() {
 	}
 }
 
-$(document).ready(function(){
-	$("#txtConfirmPassword").keyup(checkPasswordMatch);
-	$("#txtNewPassword").keyup(checkPasswordMatch);
-});
+$(document).ready(function() {
+					$("#txtConfirmPassword").keyup(checkPasswordMatch);
+					$("#txtNewPassword").keyup(checkPasswordMatch);
+					$('.delete-product').on('click', function (){
+						/*<![CDATA[*/
+					    var path = /*[[@{/}]]*/'remove';
+					    /*]]>*/
+						
+						var id=$(this).attr('id');
+						
+						bootbox.confirm({
+							message: "Are you sure to remove this product? It can't be undone.",
+							buttons: {
+								cancel: {
+									label:'<i class="fa fa-times"></i> Cancel'
+								},
+								confirm: {
+									label:'<i class="fa fa-check"></i> Confirm'
+								}
+							},
+							callback: function(confirmed) {
+								if(confirmed) {
+									$.post(path, {'id':id}, function(res) {
+										location.reload();
+									});
+								}
+							}
+						});
+					});
+				});
