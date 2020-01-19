c04927fd4293540b2cc060a574c4b3032eff9cb0
mariaalexab <49718025+mariaalexab@users.noreply.github.com>
2020-01-19 18:01:14 +0200

    Add files via upload

--- java/com/myPantry/repository/RecipeIngredientListRepository.java
+++ java/com/myPantry/repository/RecipeIngredientListRepository.java
@@ -1,10 +1,15 @@
 package com.myPantry.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.myPantry.domain.RecipeIngredientList;
 
 public interface RecipeIngredientListRepository extends CrudRepository<RecipeIngredientList, Long>{
+	@Query(nativeQuery = true, value = "SELECT * FROM recipe R INNER JOIN RecipeIngredientList RL ON R.id=RL.id")
 	RecipeIngredientList findOne(Long id);
+	
 
 }
