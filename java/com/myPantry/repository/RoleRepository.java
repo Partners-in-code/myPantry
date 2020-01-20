8d17c61344e22f6900259b51515fd9d46d784009
mariaalexab <49718025+mariaalexab@users.noreply.github.com>
2020-01-20 02:46:44 +0200

    delete unnecessary folders

--- java/com/myPantry/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myPantry.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.myPantry.domain.security.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-	Role findByname(String name);
-}
--- java/com/myPantry/service/FavoriteService.java
+++
@@ -1,9 +0,0 @@
-package com.myPantry.service;
-
-import com.myPantry.domain.Favorite;
-
-public interface FavoriteService {
-	Favorite updateFavorite(Favorite favorite);
-
-	void clearFavorite(Favorite favorite);
-}
